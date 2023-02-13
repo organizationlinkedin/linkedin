7351091c057179fa7abf7b5ec4be14e26867eb4f
gasmighassen <gasmi.ghassen@gmail.com>
2023-02-13 12:12:59 +0100

    LC-4 <create carousel>

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const App = () => (
   <BrowserRouter>
--- front/src/pages/LandingPage.tsx
+++ front/src/pages/LandingPage.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
+import Carosel from "../components/Carosel";
+
 function LandingPage() {
   return (
     <div className="container">
       <Navbar />
       <Main />
-   {/*    <Footer /> */}
+      <Carosel />
+      {/*    <Footer /> */}
     </div>
   );
 }
